06aa808a61dbd960fff334a683391416057cb4bb
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-04-03 20:40:40 +0300

    feat(Catalog): implement CreateProduct vertical slice with Carter and MediatR

--- src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace BuildingBlocks.CQRS
 {
-    public interface IQuery<out TResponse>:IRequest<TResponse>
-        where TResponse : notnull
+    public interface IQueryHandler<in TQuery,TResponse>
+        :IRequestHandler<TQuery,TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse:notnull
     {
-
     }
 }
- 
