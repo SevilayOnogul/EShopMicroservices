04f56c1aff7cdb16c29098c33e8de2c6b1777f86
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-04-13 16:40:37 +0300

    feat(Basket.API): consume Discount gRPC service when adding basket item

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -35,6 +35,21 @@ builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
     .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
+
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+    .ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        var handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+        return handler;
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -45,4 +60,4 @@ app.UseHealthChecks("/health",
     {
         ResponseWriter=UIResponseWriter.WriteHealthCheckUIResponse
     });
-app.Run();
+ app.Run();
