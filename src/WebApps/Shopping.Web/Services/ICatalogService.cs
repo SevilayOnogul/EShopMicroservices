09873f109f2c114a0a2bb096159789ffac7bc484
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-06-15 19:20:13 +0300

    feat(web): add IBasketService and register Refit clients in Program

--- src/WebApps/Shopping.Web/Services/ICatalogService.cs
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -3,12 +3,12 @@ namespace Shopping.Web.Services;
 
 public interface ICatalogService
 {
-    //[Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
+    [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
     Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
     
-    //[Get("/catalog-service/products/{id}")]
+    [Get("/catalog-service/products/{id}")]
     Task<GetProductByIdResponse> GetProduct(Guid id);
     
-    //[Get("/catalog-service/products/category/{category}")]
+    [Get("/catalog-service/products/category/{category}")]
     Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
 }
