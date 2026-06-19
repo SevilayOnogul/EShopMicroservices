82f4332ca2ab0cf2b802fa912fd0f3a6c1d2adbe
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-06-19 15:54:41 +0300

    feat(buildingblocks): add custom serilog logging extension with seq integration

---
+++ src/BuildingBlocks/BuildingBlocks/Logging/LoggingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace BuildingBlocks.Logging;
+
+public static class LoggingExtensions
+{
+    public static ConfigureHostBuilder UseCustomLogging(this ConfigureHostBuilder host, string applicationName)
+    {
+        host.UseSerilog((context, loggerConfiguration) =>
+        {
+            loggerConfiguration
+                .MinimumLevel.Information() 
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", applicationName) 
+                .Enrich.WithMachineName()
+                .Enrich.WithProcessId()
+                .WriteTo.Console() 
+                .WriteTo.Seq("http://seq:80"); 
+        });
+
+        return host;
+    }
+}
