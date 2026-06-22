43225539be453f5a35bd3b9be34eb3d649ae7494
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-06-23 00:05:36 +0300

    test: implement happy path unit test for getproductbyid handler

--- src/tests/Catalog.API.Tests/Catalog.API.Tests/Products/GetProductById/GetProductByIdHandlerTests.cs
+++ src/tests/Catalog.API.Tests/Catalog.API.Tests/Products/GetProductById/GetProductByIdHandlerTests.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Catalog.API.Tests.Products.GetProductById
+using Catalog.API.Models;
+using Catalog.API.Products.GetProductById;
+using FluentAssertions;
+using Marten;
+using Moq;
+
+namespace Catalog.API.Tests.Products.GetProductById;
+
+public class GetProductByIdHandlerTests
 {
-    internal class GetProductByIdHandlerTests
+    [Fact]
+    public async Task Handle_WhenProductExists_ShouldReturnProduct()
     {
+        var productId=Guid.NewGuid();
+        var fakeProduct = new Product
+        {
+            Id = productId,
+            Name = "Test Mock Phone",
+            Category = new List<string> { "Smart Phone" },
+            Description = "Test",
+            ImageFile = "mock.png",
+            Price = 9500
+        };
+
+        var mockSession=new Mock<IDocumentSession>();
+
+        mockSession.Setup(s=>s.LoadAsync<Product>(productId,It.IsAny<CancellationToken>())).ReturnsAsync(fakeProduct);
+
+        var query = new GetProductByIdQuery(productId);
+        var handler=new GetProductByIdQueryHandler(mockSession.Object);
+
+        var result=await handler.Handle(query,CancellationToken.None);
+
+
+        result.Should().NotBeNull();
+        result.Product.Name.Should().Be("Test Mock Phone");
+        result.Product.Id.Should().Be(productId);
+
+
+        mockSession.Verify(s=>s.LoadAsync<Product>(productId, It.IsAny<CancellationToken>()), Times.Once); 
     }
 }
