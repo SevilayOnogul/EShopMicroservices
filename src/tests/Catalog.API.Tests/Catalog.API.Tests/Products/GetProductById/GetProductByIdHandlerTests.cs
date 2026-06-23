d0644df95bedebf251e67ef38f44c4bf05ee41cc
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-06-23 15:11:13 +0300

    test: implement edge case unit test for getproductbyid when product not found

--- src/tests/Catalog.API.Tests/Catalog.API.Tests/Products/GetProductById/GetProductByIdHandlerTests.cs
+++ src/tests/Catalog.API.Tests/Catalog.API.Tests/Products/GetProductById/GetProductByIdHandlerTests.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Catalog.API.Exceptions;
 using Catalog.API.Models;
 using Catalog.API.Products.GetProductById;
 using FluentAssertions;
@@ -13,7 +14,8 @@ public class GetProductByIdHandlerTests
     [Fact]
     public async Task Handle_WhenProductExists_ShouldReturnProduct()
     {
-        var productId=Guid.NewGuid();
+        //Arrange (Hazırlık)
+        var productId =Guid.NewGuid();
         var fakeProduct = new Product
         {
             Id = productId,
@@ -31,14 +33,39 @@ public class GetProductByIdHandlerTests
         var query = new GetProductByIdQuery(productId);
         var handler=new GetProductByIdQueryHandler(mockSession.Object);
 
+        //Act (Çalıştırma)
+
         var result=await handler.Handle(query,CancellationToken.None);
 
-
+        //Assert (Doğrulama)
         result.Should().NotBeNull();
         result.Product.Name.Should().Be("Test Mock Phone");
         result.Product.Id.Should().Be(productId);
 
 
         mockSession.Verify(s=>s.LoadAsync<Product>(productId, It.IsAny<CancellationToken>()), Times.Once); 
+    }
+
+    [Fact]
+    public async Task Handle_WhenProductDoesNotExist_ShouldThrowProductNotFoundException()
+    {
+        // 1. Arrange (Hazırlık)
+        var nonExistingProductId=Guid.NewGuid();
+        var mockSession= new Mock<IDocumentSession>();
+
+        mockSession
+        .Setup(s => s.LoadAsync<Product>(nonExistingProductId, It.IsAny<CancellationToken>()))
+        .ReturnsAsync((Product)null!);
+
+        var query=new GetProductByIdQuery(nonExistingProductId);
+        var handler = new GetProductByIdQueryHandler(mockSession.Object);
+
+        // 2. Act (Çalıştırma) & 3. Assert (Doğrulama) birlikte ele alınır
+        Func<Task> act = async () => await handler.Handle(query, CancellationToken.None);
+
+        // Doğrulama aşaması: Gerçekten ProductNotFoundException fırlattı mı?
+        await act.Should().ThrowAsync<ProductNotFoundException>();
+
+
     }
 }
