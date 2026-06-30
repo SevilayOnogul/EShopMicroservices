a21b4eddb7ac6c72fb664e9d6280df2e595af17a
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-06-30 14:22:45 +0300

    test: scaffold updateorderhandler unit test directory and file structure

---
+++ src/tests/Ordering.Application.Tests/Orders/Commands/UpdateOrder/UpdateOrderHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Ordering.Application.Data;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Commands.UpdateOrder;
+using Ordering.Domain.Enums;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Tests.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderHandlerTests
+{
+   
+    
+}
