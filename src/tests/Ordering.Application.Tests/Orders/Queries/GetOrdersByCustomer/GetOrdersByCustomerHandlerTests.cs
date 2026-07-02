4ce0c1b8c0c91e7341fec45b1b78010326d584c4
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-07-02 20:55:38 +0300

    test: scaffold getordersbycustomer query unit test directory and file structure

---
+++ src/tests/Ordering.Application.Tests/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Tests.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandlerTests
+{
+}
